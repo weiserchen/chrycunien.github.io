e8b07f8d2d30ba9c7cedea36ca0430696edc5cff
woodcutter-eric <yingchiao.chen@gmail.com>
2021-11-04 16:20:38 +0800

    add page icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/planet.svg',
   organizationName: 'woodcutter-eric', // Usually your GitHub org/user name.
   projectName: 'woodcutter-eric.github.io', // Usually your repo name.
   themeConfig: {
